0a30e728a132d15933e0d4e9a6d8bd917bc10981
Marcus Camargo <75137005+Marcus-W-Camargo@users.noreply.github.com>
2026-08-30 01:23:13 -0300

    Carrega ajuste de hierarquia da Central de Ajuda

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { useDesktopKeyboardNavigation } from './hooks/useDesktopKeyboardNavigati
 import './App.css';
 import './pages/AjudaAjustes.css';
 import './pages/AjudaFaqLeitura.css';
+import './pages/AjudaHierarquia.css';
 
 function Rodape() {
   return (
