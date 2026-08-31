2fb7803b95890e24785c553fcd7eac002a9e866b
Marcus Camargo <75137005+Marcus-W-Camargo@users.noreply.github.com>
2026-08-31 13:44:12 -0300

    feat: integra política e exclusão de conta

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Lista } from './pages/Lista';
 import { Conta } from './pages/Conta';
@@ -7,66 +7,18 @@ import { ComprasSessao } from './pages/ComprasSessao';
 import { HistoricoCompras } from './pages/HistoricoCompras';
 import { Perfil } from './pages/Perfil';
 import { Ajuda } from './pages/Ajuda';
+import { Privacidade } from './pages/Privacidade';
 import { Header } from './components/Header';
 import { AjudaFeedback } from './components/AjudaFeedback';
+import { AccountDeletion } from './components/AccountDeletion';
 import { CloudAccess, CloudStatus } from './components/CloudAccess';
 import { useDesktopKeyboardNavigation } from './hooks/useDesktopKeyboardNavigation';
 import './App.css';
 import './pages/AjudaAjustes.css';
 import './pages/AjudaFaqLeitura.css';
 import './pages/AjudaHierarquia.css';
-
-function Rodape() {
-  return (
-    <footer className="rodape">
-      <p>
-        © 2026 Marcus Camargo. Todos os direitos reservados. Projeto
-        desenvolvido para fins de estudo e portfólio.
-      </p>
-    </footer>
-  );
-}
-
-function NavegacaoDesktop() {
-  useDesktopKeyboardNavigation();
-  return null;
-}
-
-function PaginaAjuda() {
-  return (
-    <>
-      <Ajuda />
-      <AjudaFeedback />
-    </>
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <NavegacaoDesktop />
-      <div className="app-layout">
-        <div className="app-conteudo">
-          <Header />
-          <CloudStatus />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/conta" element={<Conta />} />
-            <Route element={<CloudAccess />}>
-              <Route path="/lista" element={<Lista />} />
-              <Route path="/compre" element={<Compras />} />
-              <Route path="/compre/:listaId" element={<ComprasSessao />} />
-              <Route path="/historico" element={<HistoricoCompras />} />
-              <Route path="/perfil" element={<Perfil />} />
-              <Route path="/ajuda" element={<PaginaAjuda />} />
-            </Route>
-          </Routes>
-        </div>
-
-        <Rodape />
-      </div>
-    </BrowserRouter>
-  );
-}
-
+function Rodape(){return <footer className="rodape"><p>© 2026 Marcus Camargo. Todos os direitos reservados. Projeto desenvolvido para fins de estudo e portfólio. <Link to="/privacidade">Política de Privacidade</Link></p></footer>}
+function NavegacaoDesktop(){useDesktopKeyboardNavigation();return null;}
+function PaginaAjuda(){return <><Ajuda/><AjudaFeedback/></>}
+function App(){return <BrowserRouter><NavegacaoDesktop/><div className="app-layout"><div className="app-conteudo"><Header/><CloudStatus/><AccountDeletion/><Routes><Route path="/" element={<Home/>}/><Route path="/conta" element={<Conta/>}/><Route path="/privacidade" element={<Privacidade/>}/><Route element={<CloudAccess/>}><Route path="/lista" element={<Lista/>}/><Route path="/compre" element={<Compras/>}/><Route path="/compre/:listaId" element={<ComprasSessao/>}/><Route path="/historico" element={<HistoricoCompras/>}/><Route path="/perfil" element={<Perfil/>}/><Route path="/ajuda" element={<PaginaAjuda/>}/></Route></Routes></div><Rodape/></div></BrowserRouter>}
 export default App;
